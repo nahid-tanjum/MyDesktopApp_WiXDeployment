1f790eefce7b4cbb3f0cb786a0a73215368954b8
Nahid Tanjum <nahidtanzum665@gmail.com>
2024-09-11 06:13:55 +1000

    Initial commit for MyDesktopApp WiX deployment

---
+++ MyDesktopApp/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Display a simple message box with "Hello, World!"
+            MessageBox.Show("Hello, World!", "Greeting", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
